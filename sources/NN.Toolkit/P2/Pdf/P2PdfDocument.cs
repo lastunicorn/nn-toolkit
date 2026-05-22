a7027a16de989a525af5ee2c3600837ca36fec28
Alexandru Iuga <alex.iuga@gmail.com>
2026-05-22 09:28:48 +0300

    extracted a page class

--- sources/NN.Toolkit/P2/Pdf/P2PdfDocument.cs
+++ sources/NN.Toolkit/P2/Pdf/P2PdfDocument.cs
@@ -1,13 +1,21 @@
 using Tabula;
-using Tabula.Detectors;
-using Tabula.Extractors;
 using UglyToad.PdfPig;
 
 namespace DustInTheWind.NN.Toolkit.P2.Pdf;
 
+internal class ParsingStatistics
+{
+    public int PageCount { get; set; }
+
+    public int TableCount { get; set; }
+    
+    public Exception Error { get; set; }
+}
+
 internal class P2PdfDocument
 {
     private readonly string filePath;
+    private ParsingStatistics parsingStatistics;
 
     public P2PdfDocument(string filePath)
     {
@@ -21,41 +29,33 @@ internal class P2PdfDocument
             ClipPaths = true
         };
 
+        parsingStatistics = new ParsingStatistics();
         using PdfDocument pdfDocument = PdfDocument.Open(filePath, options);
+        
+        IEnumerable<P2PdfPage> pages = EnumeratePages(pdfDocument);
 
-        SimpleNurminenDetectionAlgorithm detector = new();
-        IExtractionAlgorithm algorithm = new BasicExtractionAlgorithm();
-
-        for (int pageIndex = 1; pageIndex <= pdfDocument.NumberOfPages; pageIndex++)
+        foreach (P2PdfPage page in pages)
         {
-            PageArea page = ObjectExtractor.Extract(pdfDocument, pageIndex);
-            IReadOnlyList<TableRectangle> regions = detector.Detect(page);
-
-            IEnumerable<P2PdfTable> tables = ExtractTables(page, regions, algorithm);
+            parsingStatistics.PageCount++;
+            IEnumerable<P2PdfTable> tables = page.EnumerateTables();
 
             foreach (P2PdfTable table in tables)
             {
+                parsingStatistics.TableCount++;
                 IEnumerable<P2PdfTableRow> rows = table.EnumerateRows();
-
+                
                 foreach (P2PdfTableRow row in rows)
                     yield return row;
             }
         }
     }
 
-    private static IEnumerable<P2PdfTable> ExtractTables(PageArea page, IReadOnlyList<TableRectangle> regions, IExtractionAlgorithm algorithm)
+    private IEnumerable<P2PdfPage> EnumeratePages(PdfDocument pdfDocument)
     {
-        if (regions.Count == 0)
-            yield break;
-
-        foreach (TableRectangle region in regions)
+        for (int pageIndex = 1; pageIndex <= pdfDocument.NumberOfPages; pageIndex++)
         {
-            PageArea pageArea = page.GetArea(region.BoundingBox);
-            IEnumerable<P2PdfTable> p2PdfTables = algorithm.Extract(pageArea)
-                .Select(x => new P2PdfTable(x));
-
-            foreach (P2PdfTable p2PdfTable in p2PdfTables)
-                yield return p2PdfTable;
+            PageArea pageArea = ObjectExtractor.Extract(pdfDocument, pageIndex);
+            yield return new P2PdfPage(pageArea);
         }
     }
 }
---
+++ sources/NN.Toolkit/P2/Pdf/P2PdfPage.cs
@@ -0,0 +1,37 @@
+using Tabula;
+using Tabula.Detectors;
+using Tabula.Extractors;
+
+namespace DustInTheWind.NN.Toolkit.P2.Pdf;
+
+internal class P2PdfPage
+{
+    private readonly PageArea pageArea;
+
+    public P2PdfPage(PageArea pageArea)
+    {
+        this.pageArea = pageArea ?? throw new ArgumentNullException(nameof(pageArea));
+    }
+
+    public IEnumerable<P2PdfTable> EnumerateTables()
+    {
+        SimpleNurminenDetectionAlgorithm detector = new();
+        IExtractionAlgorithm algorithm = new BasicExtractionAlgorithm();
+
+        IReadOnlyList<TableRectangle> regions = detector.Detect(pageArea);
+        
+        if (regions.Count == 0)
+            yield break;
+
+        foreach (TableRectangle region in regions)
+        {
+            PageArea tableArea = pageArea.GetArea(region.BoundingBox);
+
+            IEnumerable<P2PdfTable> p2PdfTables = algorithm.Extract(tableArea)
+                .Select(x => new P2PdfTable(x));
+
+            foreach (P2PdfTable p2PdfTable in p2PdfTables)
+                yield return p2PdfTable;
+        }
+    }
+}
